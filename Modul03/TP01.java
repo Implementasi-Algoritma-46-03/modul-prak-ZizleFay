c4281c64a9b1df610e9fc6c546cf102ed15564d5
Abdul Aziz <aziz75821@gmail.com>
2025-09-29 19:21:40 +0700

    TP 1-3

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-    }
+        System.out.println("----------");
+		System.out.println("Tabungan Dira");
+		System.out.println("Masukan 1");
+		System.out.println("==========");
+		int X = 500000;
+		int Y = 10000;
+		int N = 30;
+
+		
+		int total = X + (Y*N);
+		System.out.println(total);
+
+
+		System.out.println("==========");
+		System.out.println("Tabungan Dira");
+		System.out.println("Masukan 2");
+		System.out.println("----------");
+		int X2 = 250000;
+		int Y2 = 25000;
+		int N2 = 30;
+
+		
+		int total2 = X2 + (Y2*N2);
+		System.out.println(total2);
+	}
 }
