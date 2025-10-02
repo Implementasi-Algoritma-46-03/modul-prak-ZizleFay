8e832db1539e6480dbe72d3591c357b4956a38cb
Abdul Aziz <aziz75821@gmail.com>
2025-10-02 17:16:09 +0700

    Update

--- Modul03/Jurnal01.java
+++ Modul03/Jurnal01.java
@@ -1,6 +1,18 @@
+
+import java.util.Scanner;
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int lembarSeribu = n / 1000;
+        int sisaUang = n % 1000;
+
+        System.out.println(lembarSeribu);
+        System.out.println(sisaUang);
+
     }
 }
--- Modul03/Jurnal02.java
+++ Modul03/Jurnal02.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        double median = (a + b + c) / 3;
+        
+        System.out.printf("%.2f", median);
     }
 }
