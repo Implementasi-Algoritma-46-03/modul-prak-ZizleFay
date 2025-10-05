151e1dcb2b702ab8f0a8e87600877ebc146adf34
Abdul Aziz <aziz75821@gmail.com>
2025-10-05 22:20:05 +0700

    Update

--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
 public class TP02 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        
+        Scanner input = new Scanner(System.in);
+
+        final double PERSENTASE_PPN = 0.11;
+        final double PERSENTASE_DISKON = 0.05;
+        final int MINIMAL_TRANSAKSI_DISKON = 200000;
+
+        int N = input.nextInt();
+
+        double jumlahDiskon = 0;
+        
+        if (N > MINIMAL_TRANSAKSI_DISKON) {
+            jumlahDiskon = N * PERSENTASE_DISKON;
+        }
+
+        double totalSetelahDiskon = N - jumlahDiskon;
+        
+        double pajakPpn = totalSetelahDiskon * PERSENTASE_PPN;
+
+        double totalDibayar = totalSetelahDiskon + pajakPpn;
+
+        System.out.println("Transaksi = " + N);
+        System.out.println("Total = " + totalSetelahDiskon);
+        System.out.println("Ppn 11% = " + pajakPpn);
+        System.out.println("Total dibayar = " + totalDibayar);
+
+        input.close();
     }
 }
--- Modul04/TP03.java
+++ Modul04/TP03.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
+
 public class TP03 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int D = input.nextInt();
+        int R = input.nextInt();
+        int I = input.nextInt();
+
+        if (D > R && D > I) {
+            System.out.println("Dira");
+        } else if (R > D && R > I) {
+            System.out.println("Radi");
+        } else {
+            System.out.println("Idar");
+        }
+
+        input.close();
     }
 }
