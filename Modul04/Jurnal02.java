e1d996a5e61592a129c091482254e7e4c7cdacda
Abdul Aziz <aziz75821@gmail.com>
2025-10-09 17:49:21 +0700

    Update

--- Modul04/Jurnal02.java
+++ Modul04/Jurnal02.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Jurnal02 {
+    public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        double Pajak = 0;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        if (N <= 50)
+            Pajak = 0.05;
+        else if (N >= 51 && N <= 250)
+            Pajak = 0.15;
+        else if (N >= 251 && N <= 500)
+            Pajak = 0.25;
+        else if (N >= 501)
+            Pajak = 0.3;
+        
+        int juta = N * 1000_000;
+        double besarPajak = juta * Pajak;
+        double hasilBersih = juta - besarPajak;
+        System.out.println("Penghasilan kotor = Rp. " + N + " juta Rupiah");
+        System.out.println("Pajak " + (int)(Pajak * 100) + "% = Rp " + (int)besarPajak);
+        System.out.println("Penghasilan bersih = Rp. " +(int) hasilBersih);
     }
 }
--- Modul04/Jurnal03.java
+++ Modul04/Jurnal03.java
@@ -1,6 +1,34 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int D = input.nextInt();
+        int R = input.nextInt();
+        int I = input.nextInt();
+
+        String Dira = "Dira";
+        String Radi = "Radi";
+        String Idar = "Idar";
+
+        System.out.print("nama pemilik nilai terendah hingga tertinggi: ");
+
+        if (D <= R && R <= I) {
+            System.out.println(Dira + ", " + Radi + ", " + Idar);
+        } else if (D <= I && I <= R) {
+            System.out.println(Dira + ", " + Idar + ", " + Radi);
+        } else if (R <= D && D <= I) {
+            System.out.println(Radi + ", " + Dira + ", " + Idar);
+        } else if (R <= I && I <= D) {
+            System.out.println(Radi + ", " + Idar + ", " + Dira);
+        } else if (I <= D && D <= R) {
+            System.out.println(Idar + ", " + Dira + ", " + Radi);
+        } else { // I <= R && R <= D
+            System.out.println(Idar + ", " + Radi + ", " + Dira);
+        }
+
+
+        input.close();
     }
 }
