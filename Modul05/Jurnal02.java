a46cbb94696609b1f39717a3c21d0324500bdcc2
Abdul Aziz <aziz75821@gmail.com>
2025-10-16 18:15:30 +0700

    NewUpdate

--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,6 +1,76 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String H = scanner.nextLine();
+        int N = scanner.nextInt();
+
+        int nomorHariAwal = 0;
+        
+        // Konversi nama hari ke representasi angka (0-6)
+        switch (H.toLowerCase()) {
+            case "senin":
+                nomorHariAwal = 0;
+                break;
+            case "selasa":
+                nomorHariAwal = 1;
+                break;
+            case "rabu":
+                nomorHariAwal = 2;
+                break;
+            case "kamis":
+                nomorHariAwal = 3;
+                break;
+            case "jumat":
+                nomorHariAwal = 4;
+                break;
+            case "sabtu":
+                nomorHariAwal = 5;
+                break;
+            case "minggu":
+                nomorHariAwal = 6;
+                break;
+            default:
+                System.out.println("Nama hari tidak valid.");
+                scanner.close();
+                return;
+        }
+
+        // Menghitung nomor hari tujuan menggunakan operasi modulus
+        int nomorHariTujuan = (nomorHariAwal + N) % 7;
+
+        String namaHariTujuan = "";
+        
+        // Konversi hasil perhitungan angka kembali ke nama hari
+        switch (nomorHariTujuan) {
+            case 0:
+                namaHariTujuan = "Senin";
+                break;
+            case 1:
+                namaHariTujuan = "Selasa";
+                break;
+            case 2:
+                namaHariTujuan = "Rabu";
+                break;
+            case 3:
+                namaHariTujuan = "Kamis";
+                break;
+            case 4:
+                namaHariTujuan = "Jumat";
+                break;
+            case 5:
+                namaHariTujuan = "Sabtu";
+                break;
+            case 6:
+                namaHariTujuan = "Minggu";
+                break;
+        }
+
+        System.out.println(namaHariTujuan);
+        
+        scanner.close();
     }
 }
--- Modul05/Jurnal03.java
+++ Modul05/Jurnal03.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Jurnal03 {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+    public static void main(String[] args) {
+        // Mengatur locale agar menggunakan titik sebagai pemisah desimal
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // Meminta masukan bilangan, operator, dan bilangan
+        int A = scanner.nextInt();
+        char O = scanner.next().charAt(0);
+        int B = scanner.nextInt();
+
+        // Menggunakan switch-case untuk menentukan operasi
+        switch (O) {
+            case '+':
+                System.out.println(A + B);
+                break;
+            case '-':
+                System.out.println(A - B);
+                break;
+            case '*':
+                System.out.println(A * B);
+                break;
+            case '/':
+                // Melakukan pembagian dengan hasil bilangan riil
+                if (B != 0) {
+                    // Casting salah satu operand ke double untuk hasil desimal
+                    double hasilPembagian = (double) A / B;
+                    System.out.printf("%.7f", hasilPembagian);
+                } else {
+                    System.out.println("Error: Tidak bisa melakukan pembagian dengan nol.");
+                }
+                break;
+            default:
+                System.out.println("Operator tidak valid.");
+                break;
+        }
+        
+        scanner.close();
     }
 }
