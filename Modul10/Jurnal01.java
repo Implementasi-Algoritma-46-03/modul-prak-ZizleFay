594d7d974f2fa3e924321b6a831ab15bc98a633f
Abdul Aziz <aziz75821@gmail.com>
2025-11-20 16:15:50 +0700

    UP

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,7 +1,9 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        
         for (int i = 1; i <= 6; i++) {
+            if (i % 2 == 0) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= 5; j++) {
                 System.out.print(i + " ");
             }
@@ -9,5 +11,3 @@ public class Jurnal01 {
         }
     }
 }
-
-
