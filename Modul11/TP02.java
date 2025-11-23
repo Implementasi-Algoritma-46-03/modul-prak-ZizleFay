e2281491443330c1a97b2d62db16f497eaff32ac
Abdul Aziz <aziz75821@gmail.com>
2025-11-24 00:26:02 +0700

    up

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TP02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int jumlahPeserta = scanner.nextInt();
-        int[] poinPeserta = new int[jumlahPeserta];
-
-        for (int i = 0; i < jumlahPeserta; i++) {
-            poinPeserta[i] = scanner.nextInt();
+        int jumlahPeserta = 0;
+        
+        if (scanner.hasNextInt()) {
+            jumlahPeserta = scanner.nextInt();
         }
 
-        Arrays.sort(poinPeserta);
+        ArrayList<Integer> poinPeserta = new ArrayList<>();
 
-        for (int i = jumlahPeserta - 1; i >= 0; i--) {
-            System.out.print(poinPeserta[i]);
+        for (int i = 0; i < jumlahPeserta; i++) {
+            if (scanner.hasNextInt()) {
+                poinPeserta.add(scanner.nextInt());
+            } else {
+                break;
+            }
+        }
+
+        Collections.sort(poinPeserta);
+
+        for (int i = poinPeserta.size() - 1; i >= 0; i--) {
+            System.out.print(poinPeserta.get(i));
             if (i > 0) {
                 System.out.print(" ");
             }
